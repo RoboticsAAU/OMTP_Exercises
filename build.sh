e73f3e2b9954e9aef5d5e701f6ff1526629127f4
ToRexZ <victor.risager@hotmail.com>
2024-02-15 16:32:44 +0000

    fixed build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -e
 
 # Set the default build type
 # BUILD_TYPE=RelWithDebInfo
-colcon build --symlink-install  --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" 
+colcon build --symlink-install --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" 
         # -Wall -Wextra -Wpedantic
